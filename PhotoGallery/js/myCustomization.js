8323fc378761384627bd2616098a38e69c1d982e
suraj-mandal <suraj.mandal738@gmail.com>
2018-03-06 19:10:12 +0530

    Add files via upload

---
+++ PhotoGallery/js/myCustomization.js
@@ -0,0 +1,35 @@
+function showDogImages() {
+    if (document.getElementById("dog-img-background").style.display === "none"){
+        document.getElementById("dog-img-background").style.display = "block";
+    }
+    else {
+        document.getElementById("dog-img-background").style.display = "none";
+    }
+}
+
+function showCatImages() {
+    if (document.getElementById("cat-img-background").style.display === "none"){
+        document.getElementById("cat-img-background").style.display = "block";
+    }
+    else {
+        document.getElementById("cat-img-background").style.display = "none";
+    }
+}
+
+function showPlantImages() {
+    if (document.getElementById("plant-img-background").style.display === "none"){
+        document.getElementById("plant-img-background").style.display = "block";
+    }
+    else {
+        document.getElementById("plant-img-background").style.display = "none";
+    }
+}
+
+function showSceneryImages() {
+    if (document.getElementById("scenery-img-background").style.display === "none"){
+        document.getElementById("scenery-img-background").style.display = "block";
+    }
+    else {
+        document.getElementById("scenery-img-background").style.display = "none";
+    }
+}
